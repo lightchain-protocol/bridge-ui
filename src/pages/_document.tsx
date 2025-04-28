3598ca0364a869d3ac7f286a101ce01c5f109b05
lightchainaidev <lightchainaidev@proton.me>
2025-04-28 13:18:28 +0200

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
-import { APP_DESCRIPTION, APP_NAME, APP_URL, BRAND_COLOR, MAIN_FONT } from '../consts/app';
+import { APP_DESCRIPTION, APP_NAME, APP_URL, MAIN_FONT } from '../consts/app';
 
 export default function Document() {
   return (
@@ -7,17 +7,20 @@ export default function Document() {
       <Head>
         <meta charSet="utf-8" />
 
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color={BRAND_COLOR} />
         <link rel="shortcut icon" href="/favicon.png" />
         <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" /> */}
+
+        <link rel="icon" type="image/png" href="/logos/lcai.png" />
+        <link rel="shortcut icon" href="/logos/lcai.png" />
 
         <meta name="application-name" content={APP_NAME} />
-        <meta name="keywords" content={APP_NAME + ' Hyperlane Token Bridge Interchain App'} />
+        <meta name="keywords" content={APP_NAME + ' LCAI Token Bridge Interchain App'} />
         <meta name="description" content={APP_DESCRIPTION} />
 
         <meta name="HandheldFriendly" content="true" />
@@ -27,7 +30,7 @@ export default function Document() {
         <meta property="og:url" content={APP_URL} />
         <meta property="og:title" content={APP_NAME} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content={`${APP_URL}/logo.png`} />
+        <meta property="og:image" content={`${APP_URL}/logo.svg`} />
         <meta property="og:description" content={APP_DESCRIPTION} />
       </Head>
       <body className={`${MAIN_FONT.variable} font-sans text-black`}>
