847eee6e3dfa8d6e22d21dcc6bc2707b80e12a87
harrison-hayes <harrison@lightchain.ai>
2026-02-26 11:58:47 +0600

    feat: update footer and navigation config URLs, adjust padding in navigation styles

--- src/utils/fetchFooterConfig.ts
+++ src/utils/fetchFooterConfig.ts
@@ -1,7 +1,6 @@
 import { RawFooterConfig } from '../components/footer/types/types';
 
-const FOOTER_CONFIG_URL =
-  'https://gist.githubusercontent.com/marcuslcai/cbf1b8f7c8e63b7cf76987bd17a6dc20/raw/f0740def97e439f2effbe12e554871f531d8e9bc/lcai-footer.json';
+const FOOTER_CONFIG_URL = 'https://docs.lightchain.ai/footer-config.json';
 
 export async function fetchFooterConfig(): Promise<RawFooterConfig> {
   const res = await fetch(FOOTER_CONFIG_URL, { next: { revalidate: 3600 } });
