fe59b68cd5e4b8f0f5f251f96fc155d8e80b3c7a
lightchainaidev <lightchainaidev@proton.me>
2026-04-22 16:05:42 +0200

    Update warpRoutes.ts

--- src/consts/warpRoutes.ts
+++ src/consts/warpRoutes.ts
@@ -6,11 +6,11 @@ import { TokenStandard, WarpCoreConfig } from '@hyperlane-xyz/sdk';
 export const warpRouteConfigs: WarpCoreConfig = {
   tokens: [
     {
-      addressOrDenom: '0x00f6BC302C22067Bae909FC3adFc3cbB375eC867',
+      addressOrDenom: '0x5Ff6b59Bf2eB5fD64176650011b0d74E8A55300b',
       chainName: 'lcaitestnet',
       connections: [
         {
-          token: 'ethereum|sepolia|0xaEF7a8Fbf8cFb6ee752E66bC20DBB18020C8F8c0',
+          token: 'ethereum|sepolia|0xa4d14290Cc01ec798a7852cB1b3b314ea8F992AF',
         },
       ],
       decimals: 18,
@@ -20,12 +20,12 @@ export const warpRouteConfigs: WarpCoreConfig = {
       logoURI: '/logos/lcai.png',
     },
     {
-      addressOrDenom: '0xaEF7a8Fbf8cFb6ee752E66bC20DBB18020C8F8c0',
+      addressOrDenom: '0xa4d14290Cc01ec798a7852cB1b3b314ea8F992AF',
       chainName: 'sepolia',
       collateralAddressOrDenom: '0xCAb9A0d25d7F673E6cc05B35cd28D3e888d5D4A3',
       connections: [
         {
-          token: 'ethereum|lcaitestnet|0x00f6BC302C22067Bae909FC3adFc3cbB375eC867',
+          token: 'ethereum|lcaitestnet|0x5Ff6b59Bf2eB5fD64176650011b0d74E8A55300b',
         },
       ],
       decimals: 18,
